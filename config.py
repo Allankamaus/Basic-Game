faaaeadf6fcdfc002f8c6dc651625f02d394f03f
Allankamaus <allannjoroge.kamau@strathmore.edu>
2025-07-03 12:37:08 +0300

    fixed the movement

--- config.py
+++ config.py
@@ -4,6 +4,31 @@ PLAYER_LAYER = 2
 GROUND_LAYER = 1
 
 TILESIZE = 32
+PLAYER_SPEED = 3
 
 RED = (255,0,0)
+BLUE = (0,0,255)
+GREEN = (0,255,0)
 FPS = 60
+
+BLOCK_LAYER = 1
+PLAYER_LAYER = 2
+
+tilemap = [
+'BBBBBBBBBBBBBBBBBBBB',  
+'B..................B',
+'B........BB........B',
+'B..................B',
+'B.BBB..............B',
+'B..................B',
+'B............B.....B',
+'B..................B',
+'B..................B',
+'B..................B',
+'B..................B',
+'B...............BBBB',
+'B..................B',
+'B..................B',
+'BBBBBBBBBBBBBBBBBBBB',
+
+]
